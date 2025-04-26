b1bf211e03c154d9a852c3fdc4fbdf9ada2aa89b
deo <deiord44santiagoov@gmail.com>
2025-04-26 21:03:17 +0200

    mariadb runnig but incomplete

--- srcs/requirements/mariadb/tools/mdb_start.sh
+++ srcs/requirements/mariadb/tools/mdb_start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Lanzar el servidor como usuario mysql
-exec su-exec mysql mysqld --datadir=/var/lib/mysql --user=mysql
+exec su-exec mysql mysqld --port=$MARIADB_PORT --datadir=/var/lib/mysql --user=mysql
