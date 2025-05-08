51e83353d3208f8a27c3b0b890c677ac7e5e25b7
deo <deiord44santiagoov@gmail.com>
2025-05-08 16:54:34 +0200

    still not working buuut better

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -8,24 +8,14 @@ if ($db_password === false)
 	exit;
 }
 
-define( 'DB_NAME', 'mydb' );
-define( 'DB_USER', 'deordone') );
-define( 'DB_PASSWORD', 'h1234' );
-define( 'DB_HOST', 'mariadb:3306' );
-define( 'DB_CHARSET', 'utf8') );
-define( 'DB_COLLATE', '' );
-
-$table_prefix = 'wp_';
- 
-/*
 define( 'DB_NAME', getenv('WP_DB') );
 define( 'DB_USER', getenv('WP_DB_USER') );
 define( 'DB_PASSWORD', "$db_password" );
 define( 'DB_HOST', getenv('WP_HOST') );
 define( 'DB_CHARSET', getenv('WP_CHARSET') );
 define( 'DB_COLLATE', '' );
-$table_prefix = getenv('WP_PREFIX');
- */
+
+$table_prefix = getenv('WP_PREFIX'); 
  
 /**
  * For developers: WordPress debugging mode.
--- srcs/requirements/wordpress/tools/wp_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wp_entrypoint.sh
@@ -2,30 +2,65 @@
 
 set -e
 
+connection_loop()
+{
+	service=$1
+	port=$2
+	until nc -z -v -w30 $service $port; do
+	  echo "Waiting for $service it is available on the network..."
+	  sleep 5
+	done
+	echo "Maria DB Ready"
+}
 
-until nc -z -v -w30 mariadb 3306; do
-  echo "Esperando a que MariaDB esté disponible en la red..."
-  sleep 30
-done
+download_wp()
+{
+	volume_path=$1
+	if [ ! -f $volume_path/index.php ]; then
+	echo "Installing Wordpress"
+	cd $volume_path && \
+	curl -O https://wordpress.org/wordpress-${WP_VERSION}.tar.gz > /dev/null 2>&1 && \
+	tar -xzf wordpress-${WP_VERSION}.tar.gz --strip-components=1 && \
+	rm wordpress-${WP_VERSION}.tar.gz
+	fi
+	echo "Wordpress Instaled!"
+}
 
-echo "listo parce"
+add_group()
+{
+	group=www-data
+	user=www-data
+	if ! getent group "$group" > /dev/null 2>&1; then
+	addgroup -S www-data
+	fi 
+	if ! getent passwd "$user" > /dev/null 2>&1; then
+	adduser -S -G www-data www-data
+	fi
+	chown -R www-data:www-data /var/www/html
+}
 
-#DOWNLOADING WORDPRESS!
-#if [ ! -f /var/www/html/index.php ]; then
-#	cd /var/www/html && \
-#	curl -O https://wordpress.org/wordpress-${WP_VERSION}.tar.gz && \
-#	tar -xzvf wordpress-${WP_VERSION}.tar.gz && \
-#	rm wordpress-${WP_VERSION}.tar.gz
-#	mv /wp-config.php /var/www/html/wordpress/wp-config.php
-#	#Change permission
-#	chmod -R 755 /var/www/html
-#
-#	#LISTEN CONNECTIONS
-#	sed -i 's/^listen = 127\.0\.0\.1:9000/listen = 0.0.0.0:9000/' /etc/php82/php-fpm.d/www.conf
-#
-#fi
+conf_php()
+{
+	php_version=$1
+	conf_file=/etc/$1/php-fpm.d/www.conf
+	sed -i 's/^listen = 127\.0\.0\.1:9000/listen = 0.0.0.0:9000/' $conf_file
+	sed -i 's/^user = nobody/user = www-data/' $conf_file
+	sed -i 's/^group = nobody/group = www-data/' $conf_file
+	sed -i 's/^;clear_env = no/clear_env = no/' $conf_file
+	echo "php conf complete!"
+	if [ -f /wp-config.php ]; then
+		mv /wp-config.php $volume_path/
+	fi
+}
 
-#el mv no se tiene que hacer siempre
-mv /wordpress/* /var/www/html/
+init()
+{
+	connection_loop "mariadb" "3306"
+	download_wp "/var/www/html"
+	add_group
+	conf_php "${PHP_VERSION}"
+}
+
+init
 
 exec "$@" -F
